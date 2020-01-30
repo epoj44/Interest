8db1f592ea28287010f716b91f59073d14321ba4
epoj <ferenczy.domonkos@gmail.com>
2020-01-30 13:21:18 +0100

    X axis begins from 1; graph code minimized

--- src/modules/Graph.js
+++ src/modules/Graph.js
@@ -7,7 +7,7 @@ class Graph extends React.Component {
     render(){
         
         // Is there other options than variables?
-        let datums, endline, endpoint, points = [], circles = []
+        let points = [], circles = []
         if (this.props.data !== undefined){
 
             // It shouldn't be here...?
@@ -17,81 +17,53 @@ class Graph extends React.Component {
             this.distY = Math.round((this.maxY + this.minY) / 9)
             this.distX = 900 / this.length;
 
-            datums = this.props.data
-            .filter((_, index) => index < this.length - 1)
-            .map((_, index) => {
-
-                circles.push(
-                <circle
-                    key={"P"+index+1}
-                    cx={(index+1) * this.distX +75}
-                    cy={this.props.data[index+1].total / this.distY * 50 +25}
-                    r="1"
-                    stroke="#00496a"
-                    strokeWidth="3"
-                />)
-
+            circles = this.props.data.map((_, index) => {
                 points.push(
                     index * this.distX +75 + ", " +
                     (this.props.data[index].total / this.distY * 50 +25)
                 )
 
                 return (
-                <line
-                    key={index}
-                    index={index}
-                    x1={index * this.distX +75}
-                    y1={this.props.data[index].total / this.distY * 50 +25}
-                    x2={(index+1) * this.distX +75}
-                    y2={this.props.data[index+1].total / this.distY * 50 +25}
-                    style={{stroke:"#00496a",strokeWwidth:"2"}}
+                <circle
+                    key={"P"+index}
+                    cx={index * this.distX +75}
+                    cy={this.props.data[index].total / this.distY * 50 +25}
+                    r="1"
+                    stroke="#00496a"
+                    strokeWidth="3"
                 />)
-
               })
               points.push(
-                (this.length -1) * this.distX +75 + ", " + 
-                (this.props.data[this.length -1].total / this.distY * 50 +25),
-
-                (this.length -1) * this.distX +75 + ", " + 
-                (25),
-                
-                75, 25
+                (this.length -1) * this.distX +75 + ", " + (25),
+                75 + "," + 25
               )
-
-            endline = 
-            <line 
-                x1={(this.length -1) * this.distX +75}
-                y1={this.props.data[this.length -1].total / this.distY * 50 +25}
-                x2={75}
-                y2={this.props.data[this.length -1].total / this.distY * 50 +25}
-                style={{stroke:"#252525",strokeDasharray:"4"}}
-            />
-
-            endpoint = 
-            <text
-                key="endpoint" x={500}
-                y={(-1* this.props.data[this.length -1].total / this.distY * 50 - 5) -25}
-                fontSize="16"
-                style={{transform: "rotateX(0)!important",stroke:"#00496a",fill:"#00496a",fontSize:"18px"}}>
-                {convertToString(this.props.data[this.length -1].total)}
-            </text>
-            
-        } else {
-            datums = ""
-        }
         return (
             <div className="frame">
                 <svg className="chart" viewBox="0 0 1000 500" style={{"transform": "rotateX(180deg)"}}>
                     <Grid maxY={this.maxY} minY={this.minY} numbers={this.length} distX={this.distX}></Grid>
                     <polygon points={points} style={{fill: "rgba(0, 73, 106, 0.4)",strokeWidth: 1,stroke: "rgb(0, 73, 106)"}} />
-                    {datums}
                     {circles}
-                    {endline}
-                    {endpoint}
+                    <line 
+                        x1={(this.length -1) * this.distX +75}
+                        y1={this.props.data[this.length -1].total / this.distY * 50 +25}
+                        x2={75}
+                        y2={this.props.data[this.length -1].total / this.distY * 50 +25}
+                        style={{stroke:"#252525",strokeDasharray:"4"}}
+                    />
+                    <text
+                        key="endpoint" x={500}
+                        y={(-1* this.props.data[this.length -1].total / this.distY * 50 - 5) -25}
+                        fontSize="16"
+                        style={{transform: "rotateX(0)!important",stroke:"#00496a",fill:"#00496a",fontSize:"18px"}}>
+                        {convertToString(this.props.data[this.length -1].total)}
+                    </text>
                 </svg>
             </div>
         )
+    } else {
+        return ""
     }
+}
 }
 
 
--- src/modules/Grid.js
+++ src/modules/Grid.js
@@ -18,7 +18,7 @@ class Grid extends React.Component {
         // Vertical lines
         let amountH = this.props.numbers;
         this.distX = 900 / this.props.numbers
-        for(let i = 2; i <= amountH + 4; i++){
+        for(let i = 1; i <= amountH + 4; i++){
             if ((amountH > 40 && i%5 === 0) || (amountH <= 40)){
             grid.push(<line key={"H"+i} x1={(i * this.distX) - this.distX +75} y1="25" x2={(i * this.distX) - this.distX +75} y2="500" style={{fill:"none",stroke:"rgb(200, 200, 200)",strokeWidth:1}}></line>)
             grid.push(<text key={"VH"+i} x={(i * this.distX) - ((i > 9)?10:5) - this.distX +75} y="-5" fontSize="16" style={{transform: "rotateX(-180deg)"}}>{i}</text>)
